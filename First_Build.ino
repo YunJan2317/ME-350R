be55daea58a8720c8c145084ac80c73c2ba69c7e
YunJan2317 <112342233+YunJan2317@users.noreply.github.com>
2023-04-21 13:06:08 -0500

    Add files via upload

---
+++ First_Build.ino
@@ -0,0 +1,104 @@
+/*
+  ME 350R: First Build
+*/
+
+#define enA 9
+#define in1 4
+#define in2 5
+
+#define enB 10
+#define in3 6
+#define in4 7
+
+#define enIn 11
+#define in5 3
+#define in6 2
+
+
+const int inputSpeed = 42;
+const int driveSpeed = 42;
+const int duration = 42;
+
+int xAxis, yAxis, xMapped, yMapped, rightVelocity, leftVelocity;
+int buttonVal;
+
+
+void setup() {
+	Serial.begin(9600);
+
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+
+void loop() {
+  buttonVal = digitalRead(8);
+
+  if (buttonVal == 1) {
+    parallelParking(true);
+  }
+
+  xAxis = analogRead(A0)/2 - 255; // Read Joysticks X-axis.
+  yAxis = analogRead(A1)/2 - 255; // Read Joysticks Y-axis.
+
+  rightVelocity = (abs(yAxis - xAxis) < 255) ? yAxis - xAxis : sign(yMapped - xAxis)*255;
+  leftVelocity = (abs(yAxis + xAxis) < 255) ? yAxis + xAxis : sign(yMapped + xAxis)*255;
+
+  rightVelocity = abs(rightVelocity) < 16 ? 0 : rightVelocity;
+  leftVelocity = abs(leftVelocity) < 16 ? 0 : leftVelocity;
+
+  Serial.println(rightVelocity);
+  Serial.println(leftVelocity);
+
+  wheelControl(rightVelocity, leftVelocity);
+}
+
+
+void wheelControl(int rightVelocity, int leftVelocity) {
+
+  int rightDir, leftDir, rightSpeed, leftSpeed;
+
+  rightDir = (rightVelocity > 0) ? HIGH : LOW;
+  leftDir = (leftVelocity > 0) ? HIGH : LOW;
+
+  rightSpeed = abs(rightVelocity);
+  leftSpeed = abs(leftVelocity);
+
+  digitalWrite(in1, rightDir);
+  digitalWrite(in2, 1 - rightDir);
+  digitalWrite(in3, leftDir);
+  digitalWrite(in4, 1 - leftDir);
+  analogWrite(enA, rightSpeed);
+  analogWrite(enB, leftSpeed);
+}
+
+
+int sign(int x) {
+    return (x > 0) - (x < 0);
+}
+
+
+void parallelParking(bool dir) {
+  wheelControl(driveSpeed, driveSpeed);
+
+  if (dir) {
+    digitalWrite(in5, 1);
+    digitalWrite(in6, 0);
+  }
+  else {
+    digitalWrite(in5, 0);
+    digitalWrite(in6, 1);
+  }
+
+  analogWrite(enIn, inputSpeed);
+
+  delay(duration);
+
+  wheelControl(0, 0);
+  analogWrite(enIn, 0);
+
+}
